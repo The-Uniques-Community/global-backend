faf8ae930ad43321eca102d9a0a4cd933dde9a7d
Nemmen <aryankammboz@gmail.com>
2025-03-02 23:37:52 +0530

    S

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ import express from 'express';
 import dbConnect from './config/dbConfig.js';
 import router from './routes/routes.js';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 dbConnect();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
+const PORT = process.env.PORT || 5000;
+app.use(cors())
 app.use(express.json());
 app.use("/api", router);
 
