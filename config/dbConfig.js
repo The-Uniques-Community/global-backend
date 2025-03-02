e53a33969964d8e8345f5148c0ab30fc438062e4
Nemmen <aryankammboz@gmail.com>
2025-03-03 00:14:53 +0530

    acs

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -4,7 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 const dbConnect = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(process.env.MONGODB_URI ,{
+            useNewUrlParser: true,
+        });
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
