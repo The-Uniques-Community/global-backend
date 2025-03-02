c23784df42c78fdcc5af7a1a0f43125eaef7472e
Nemmen <aryankammboz@gmail.com>
2025-03-02 23:43:44 +0530

    asa

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json());
 app.use("/api", router);
-
+app.get("/", (req, res) => {
+    res.send("Agge dekh chlta ban");
+});
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 }
